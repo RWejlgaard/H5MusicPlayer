b9b21fd09a320d04065a79cae7059a94171179a1
Rasmus Wejlgaard <zrw@kmd.dk>
2017-04-26 13:31:11 +0200

    Implemented drag and drop

--- MusicPlayer/MainWindow.xaml.cs
+++ MusicPlayer/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MusicPlayer.Properties;
 
 namespace MusicPlayer
 {
@@ -20,9 +24,62 @@ namespace MusicPlayer
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        public ObservableCollection<Song> Songs = new ObservableCollection<Song>();
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
+        }
+
+        public class Song {
+            private string path;
+            private string title;
+            private string artist;
+            private int duration;
+
+            public Song(string path, string title, string artist, int duration)
+            {
+                this.path = path;
+                this.title = title;
+                this.artist = artist;
+                this.duration = duration;
+            }
+
+            public Song()
+            {
+                this.path = "null";
+                this.title = "null";
+                this.artist = "null";
+                this.duration = 0;
+            }
+        }
+
+        private void SongList_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                var files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+                foreach (var file in files)
+                {
+                    if (Regex.IsMatch(file, ".mp3$"))
+                    {
+
+                        SongList.Items.Add(new Song(file,"title", "Miley Cyrus", 120));
+                    }
+                }
+                Settings.Default.Save();
+                Settings.Default.Reload();
+            }
+        }
+
+        private void SongList_OnMouseRightButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Settings.Default.Songs.Clear();
+            Settings.Default.Save();
+            Settings.Default.Reload();
         }
     }
 }
